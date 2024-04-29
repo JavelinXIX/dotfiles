534269a9b424adc4b69b8f94a6ab2aef522916d4
JavelinXIX <asgar.fortissimo.1125@gmail.com>
2024-04-29 14:56:07 +0900

    add template.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,6 +14,19 @@ return {
         config = function()
             require('toggleterm').setup()
         end
+    },
+    {
+        'glepnir/template.nvim',
+        cmd = {'Template','TemProject'},
+        config = function()
+            require('template').setup({
+                temp_dir = "~/Documents/github/template/files"
+            })
+        end
     }
+
+-- lazy load you can use cmd or ft. if you are using cmd to lazyload when you edit the template file
+-- you may see some diagnostics in template file. use ft to lazy load the diagnostic not display
+-- when you edit the template file.
 }
 
